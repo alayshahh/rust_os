855ca67bb0d1b502305eea99ab905572d4096cfa
Alay Shah <47639000+alayshahh@users.noreply.github.com>
2022-01-10 23:19:21 -0500

    finsihed blog post #2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
 #![no_std] // unlinking the rust std library
+#![no_main]
 /*
 Once the std library is unlinked, there is no start atribute
 What does this mean?
@@ -11,13 +12,20 @@ crt0 sets up the env for a C application
 then this envokes the rust runtime, which sets a few more things up, then finally the main method is called
 Since we dont want to use the std library, we will now need to create our own _start function to set up the runtime for the OS & thus we have no main method just yet
 */
-#![no_main]
 
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
+	let vga_buffer = 0xb8000 as *mut u8;
+	for (i, &byte) in HELLO.iter().enumerate() {
+		unsafe{
+			*vga_buffer.offset(i as isize *2) = byte;
+			*vga_buffer.offset(i as isize *2 +1) = 0xb;
+		}
+	}
     loop {}
 }
 
